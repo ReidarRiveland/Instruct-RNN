c7db64d02ae1eb9f0c38ea7a25a22ba70123fb44
ReidarRiveland <reidar.riveland@gamil.com>
2022-09-15 14:34:42 +0200

    updates to ccgp

--- instructRNN/plotting/plot_figs.py
+++ instructRNN/plotting/plot_figs.py
@@ -4,7 +4,7 @@ from instructRNN.models.full_models import *
 from instructRNN.tasks.tasks import *
 from instructRNN.tasks.task_factory import *
 
-# plot_avg_holdout_curve('7.20models', 'swap', [ 'bertNet_lin'])
+plot_avg_holdout_curve('7.20models', 'swap', [ 'simpleNet', 'bowNet_lin', 'bowNet', 'gptNetXL_tuned', 'sbertNet_lin_tuned'])
 
 
 # plot_avg_holdout_curve('7.20models', 'swap', 
@@ -15,27 +15,25 @@ from instructRNN.tasks.task_factory import *
 
 
 
-EXP_FILE = '7.20models/swap_holdouts'
-bowNet = BoWNet(LM_out_dim=64, rnn_hidden_dim=256)
-from instructRNN.analysis.model_analysis import get_instruct_reps
+# EXP_FILE = '7.20models/swap_holdouts'
+# bowNet = BoWNet(LM_out_dim=64, rnn_hidden_dim=256)
+# from instructRNN.analysis.model_analysis import get_instruct_reps
 
 
 
+# EXP_FILE = '7.20models/swap_holdouts'
+# sbertNet = SBERTNet_lin_tuned(LM_out_dim=64, rnn_hidden_dim=256)
+# holdouts_file = 'swap0'
+# sbertNet.load_model(EXP_FILE+'/'+holdouts_file+'/'+sbertNet.model_name, suffix='_seed0')
+
+# plot_tuning_curve(sbertNet, ['Go', 'AntiGo', 'GoMod1', 'AntiGoMod1', 'AntiGoMod2', 'GoMod2'], 13, [140]*6, np.linspace(0, 2*np.pi, 100), num_repeats=1, smoothing=0.8)
 
 
-EXP_FILE = '7.20models/swap_holdouts'
-sbertNet = SBERTNet_lin_tuned(LM_out_dim=64, rnn_hidden_dim=256)
-holdouts_file = 'swap0'
-sbertNet.load_model(EXP_FILE+'/'+holdouts_file+'/'+sbertNet.model_name, suffix='_seed0')
-
-plot_tuning_curve(sbertNet, ['Go', 'AntiGo', 'GoMod1', 'AntiGoMod1', 'AntiGoMod2', 'GoMod2'], 13, [140]*6, np.linspace(0, 2*np.pi, 100), num_repeats=1, smoothing=0.8)
+# plot_scatter(sbertNet, ['DM', 'AntiDM', 'DMMod1', 'AntiDMMod1', 'DMMod2', 'AntiDMMod2'], dims=3, pcs=[1, 2, 3], num_trials=100)
+# plot_tuning_curve(sbertNet, ['DM', 'AntiDM'], 23, [140]*2, np.linspace(-0.5, 0.5, 100), num_repeats=1)
 
 
-plot_scatter(sbertNet, ['DM', 'AntiDM', 'DMMod1', 'AntiDMMod1', 'DMMod2', 'AntiDMMod2'], dims=3, pcs=[1, 2, 3], num_trials=100)
-plot_tuning_curve(sbertNet, ['DM', 'AntiDM'], 23, [140]*2, np.linspace(-0.5, 0.5, 100), num_repeats=1)
-
-
-plot_tuning_curve(sbertNet, ['DM', 'AntiDM', 'DMMod1', 'AntiDMMod1', 'DMMod2', 'AntiDMMod2'], 43, [140]*6, np.linspace(0, 2*np.pi, 100), num_repeats=1)
+# plot_tuning_curve(sbertNet, ['DM', 'AntiDM', 'DMMod1', 'AntiDMMod1', 'DMMod2', 'AntiDMMod2'], 43, [140]*6, np.linspace(0, 2*np.pi, 100), num_repeats=1)
 
 
 
@@ -44,13 +42,14 @@ plot_tuning_curve(sbertNet, ['DM', 'AntiDM', 'DMMod1', 'AntiDMMod1', 'DMMod2', '
 
 EXP_FILE = '7.20models/swap_holdouts'
 sbertNet = SBERTNet_lin_tuned(LM_out_dim=64, rnn_hidden_dim=256)
-holdouts_file = 'swap4'
+holdouts_file = 'swap6'
 sbertNet.load_model(EXP_FILE+'/'+holdouts_file+'/'+sbertNet.model_name, suffix='_seed0')
 
 #plot_tuning_curve(sbertNet, ['Go', 'AntiGo'], 32, [140]*6, np.linspace(0, 2*np.pi, 100), num_repeats=1, smoothing=1)
-plot_scatter(sbertNet, ['GoMod1', 'AntiGoMod1',  'GoMod2', 'AntiGoMod2'], dims=3, pcs=[1, 2, 3], num_trials=50)
-plot_scatter(sbertNet, ['Go', 'AntiGo', 'GoMod1', 'AntiGoMod1',  'GoMod2', 'AntiGoMod2'], dims=3, pcs=[1, 2, 3], num_trials=50, rep_depth='full')
+plot_scatter(sbertNet, ['Go', 'AntiGo', 'GoMod1', 'AntiGoMod1',  'GoMod2', 'AntiGoMod2'], dims=3, pcs=[0, 1, 2], num_trials=50)
+plot_scatter(sbertNet, ['Go', 'AntiGo', 'GoMod1', 'AntiGoMod1',  'GoMod2', 'AntiGoMod2'], dims=3, pcs=[0, 1, 2], num_trials=50, rep_depth='full')
 
+plot_scatter(sbertNet, ['MultiDM', 'AntiMultiDM', 'DMMod1', 'AntiDMMod1',  'DMMod2', 'AntiDMMod2'], dims=3, pcs=[0, 1, 2], num_trials=50)
 
 
 EXP_FILE = '7.20models/swap_holdouts'
