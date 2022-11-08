c042cdb78284d5b9c29b686068afcc5941a69906
ReidarRiveland <reidar.riveland@gmail.com>
2022-11-08 21:31:45 +0100

    ibid

--- instructRNN/trainers/model_trainer.py
+++ instructRNN/trainers/model_trainer.py
@@ -331,7 +331,7 @@ def tune_model(exp_folder, model_name, seed, labeled_holdouts, overwrite=False, 
 
     if 'gptNetXL' in model_name:
         tuning_config = TrainerConfig(file_name+'/'+model_name, seed, holdouts=holdouts, batch_len=64,
-                                            epochs=120, min_run_epochs=5, init_lr=1e-4, init_lang_lr=5e-5, scheduler_gamma=0.99,
+                                            epochs=120, min_run_epochs=5, init_lr=1e-5, init_lang_lr=1e-5, scheduler_gamma=0.99,
                                             save_for_tuning_epoch=np.nan, 
                                             **train_config_kwargs)
 
