9e00a93289b7f254802b54bb78887b92b9edb2dd
ReidarRiveland <reidar.riveland@gamil.com>
2023-03-22 15:30:15 +0100

    increase info duration

--- instructRNN/models/sensorimotor_models.py
+++ instructRNN/models/sensorimotor_models.py
@@ -102,7 +102,7 @@ class BaseNet(nn.Module):
         task_info_block[:, onset:onset+duration, :] = task_info
         return task_info_block
 
-    def forward(self, x, task_info, info_duration=10, info_onset=0): 
+    def forward(self, x, task_info, info_duration=20, info_onset=0): 
         h0 = self.__initHidden__(x.shape[0])
         task_info_block = self.expand_info(task_info, info_duration, info_onset)
         rnn_ins = torch.cat((task_info_block.to(self.__device__), x.type(torch.float32)), 2)
