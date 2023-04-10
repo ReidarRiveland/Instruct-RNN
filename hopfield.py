53d81b50d4723823487e05eae7f9e208d995b55f
ReidarRiveland <reidar.riveland@gamil.com>
2023-04-10 11:55:28 +0200

    hopfiled with rl inhibit

--- hopfield.py
+++ hopfield.py
@@ -14,14 +14,44 @@ import matplotlib.pyplot as plt
 
 EXP_FILE = '3.16models/swap_holdouts'
 simpleNet = SimpleNetPlus(rule_encoder_hidden=128)
-holdouts_file = 0
+holdouts_file = 9
 simpleNet.load_model(EXP_FILE+'/swap'+str(holdouts_file)+'/'+simpleNet.model_name, suffix='_seed0')
 
-_rule_encoding_set = get_rule_embedder_reps(simpleNet, depth=-1).mean(1)
+_rule_encoding_set = get_rule_embedder_reps(simpleNet, depth=-1)
 rule_encoding_set = np.delete(_rule_encoding_set, np.array([TASK_LIST.index(holdout_task) for holdout_task in SWAP_LIST[holdouts_file]]), axis=0)
 in_tasks = [task for task in TASK_LIST if task not in SWAP_LIST[holdouts_file]]
 
-#plot_scatter(simpleNet, ['Go', 'AntiGo', 'RTGo', 'AntiRTGo'], dims=3, rep_depth='full', num_trials=100)
+#plot_scatter(simpleNet, ['DMMod1', 'AntiDMMod1', 'DMMod2', 'AntiDMMod2'], dims=3, rep_depth='full', num_trials=100)
+
+from sklearn.decomposition import PCA
+embedder = PCA()
+embedder.fit(rule_encoding_set.reshape(-1, 128))
+componenets = embedder.components_[:20, :]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def softmax(x, beta=1):
     return np.exp(beta*x)/np.sum(np.exp(beta*x))
@@ -40,12 +70,6 @@ def recall_mhopf_w_inhibition(pattern, last_pattern, all_encodings, beta=10.0):
 
     softmaxed = softmax(np.maximum(dotted-last_dotted, 0), beta=beta)
     return np.matmul(softmaxed, all_encodings)
-
-def test_recall(model, task, beta = 10.0, noise = 0.1):
-    init_pattern = _rule_encoding_set[TASK_LIST.index(task), :] + np.random.normal(scale = noise, size=128)
-    test_recalled = recall_mhopf(init_pattern, rule_encoding_set, beta=10.0)
-    info_embedded = model.rule_encoder.rule_layer2(torch.tensor(test_recalled).float())[None, :].repeat(50, 1)
-    return task_eval_info_embedded(model, task, 50, info_embedded=info_embedded)
 
 def make_periodic_beta(max_beta, phase, num_cycles, num_points):
     return max_beta/2*np.cos(phase*np.linspace(0, 2*num_cycles*np.pi, num_points+1))+max_beta/2
@@ -83,6 +107,12 @@ def plot_memory_trace(trace, betas):
 
     plt.show()
 
+def test_recall(model, task, beta = 10.0, noise = 0.1):
+    init_pattern = _rule_encoding_set[TASK_LIST.index(task), :] + np.random.normal(scale = noise, size=128)
+    test_recalled = recall_mhopf(init_pattern, rule_encoding_set, beta=10.0)
+    info_embedded = model.rule_encoder.rule_layer2(torch.tensor(test_recalled).float())[None, :].repeat(50, 1)
+    return task_eval_info_embedded(model, task, 50, info_embedded=info_embedded)
+
 def get_recalled_tasks(recalls): 
     recalled_sims = cosine_similarity(recalls, rule_encoding_set)
     recalled_tasks = [in_tasks[ind] for ind in np.argmax(recalled_sims, axis=1)]
@@ -105,26 +135,14 @@ def get_compositional_mem_perf(recalls, task, num_trials=1):
         task_perf.append(task_eval_info_embedded(simpleNet, task, num_trials, info_embedded))
     return task_perf
 
-def numpy_ewma_vectorized(data, window):
+def update_value_weights(W_v, embedding, reward, alpha=0.01): 
+    rpe = reward-np.dot(W_v.T, embedding)
+    return W_v + (alpha*abs(rpe))*(rpe*embedding)
 
-    alpha = 2 /(window + 1.0)
-    alpha_rev = 1-alpha
-
-    scale = 1/alpha_rev
-    n = data.shape[0]
-
-    r = np.arange(n)
-    scale_arr = scale**r
-    offset = data[0]*alpha_rev**(r+1)
-    pw0 = alpha*alpha_rev**(n-1)
-
-    mult = data*pw0*scale_arr
-    cumsums = mult.cumsum()
-    out = offset + cumsums*scale_arr[::-1]
-    return out
-
-def update_value_weights(W_v, embedding, reward, alpha=0.1): 
-    return W_v + alpha*(reward-np.dot(W_v, embedding))*embedding
+def weibel(x): 
+    lam=0.5
+    k=8
+    return 1-np.exp(-(x/lam)**k)
 
 def value_sim(recalls, task, init_embedding): 
     task_perf = []
@@ -132,18 +150,20 @@ def value_sim(recalls, task, init_embedding):
     i = 2
     comp_rep = init_embedding
 
-    for _ in range(10000): 
+    for _ in range(2000): 
         if i == recalls.shape[0]: 
             print('broke')
             break
 
-        info_embedded = simpleNet.rule_encoder.rule_layer2(torch.tensor(comp_rep).float())[None, :]
-        reward = task_eval_info_embedded(simpleNet, task, 1, info_embedded)
-        task_perf.append(reward)
-        W_v = update_value_weights(W_v, comp_rep, reward)
+        for _ in range(5):
+            info_embedded = simpleNet.rule_encoder.rule_layer2(torch.tensor(comp_rep).float())[None, :]
+            reward = task_eval_info_embedded(simpleNet, task, 1, info_embedded)
+            task_perf.append(reward)
+
+        W_v = update_value_weights(W_v, comp_rep, np.mean(task_perf[-5:]), alpha=0.05-((0.05/recalls.shape[0])*i))
         value = np.dot(W_v.T, comp_rep)
         print(value)
-        stay = np.random.binomial(1, sigmoid(value))
+        stay = np.random.binomial(1, weibel(np.maximum(value, 0)))
 
         if not stay:
             print('SWITCHED')
@@ -152,44 +172,38 @@ def value_sim(recalls, task, init_embedding):
 
     return np.array(task_perf), W_v
 
-max_beta = 50
-trace, recalls, betas = get_memory_trace(max_beta, 1.0, 10, 1000, 'RTGo', use_inhibition=True)
-plot_memory_trace(trace, betas)
-
-recalled_task, perf = get_recalled_performance(recalls)
-
-
-comp_perf =- get_compositional_mem_perf(recalls, task)
-
-perf
-
-perf, W_v = value_sim(recalls, 'AntiDMMod2', _rule_encoding_set[TASK_LIST.index('AntiDMMod2')])
-
-plt.plot(perf[-100:])
-plt.show()
-plt.plot(numpy_ewma_vectorized(perf, 5))
-plt.show()
-
-sigmoid(1)
-moving_averages(np.array(perf))
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
 
 
 
+max_beta = 100
+trace, recalls, betas = get_memory_trace(max_beta, 1.0, 1000, 100_000, 'AntiGo', use_inhibition=True)
+#plot_memory_trace(trace, betas)
+#recalled_task, perf = get_recalled_performance(recalls)
+#comp_perf = get_compositional_mem_perf(recalls, 'AntiGo', num_trials=50)
+# plt.plot(comp_perf)
+# plt.ylim(0, 1.0)
+# plt.show()
 
 
 
-
-
-
-recalled_tasks = get_recalled_tasks(recalls)
-len(set(recalled_tasks))
-
-perf = get_compositional_mem_perf(recalls, 'RTGo', 50)
-plt.plot(perf)
+x = np.linspace(0, 5, 100)
+plt.plot(x, weibel(x))
 plt.show()
 
 
 
+perf, W_v = value_sim(recalls, 'AntiGo', _rule_encoding_set[TASK_LIST.index('AntiGo')])
 
-get_recalled_performance(recalls)
+from scipy.ndimage.filters import gaussian_filter1d
+
+
+
+plt.plot(moving_average(perf[3000:], 20))
+plt.plot(gaussian_filter1d(moving_average(perf[3000:], 20), sigma=500.0), color='red')
+plt.show()
+
+
+not np.random.binomial(1, 0.99)
 
