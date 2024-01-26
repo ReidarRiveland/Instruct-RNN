c5f6e446aa7360f6a296dfb8e8d38d2d2d02fefa
ReidarRiveland <reidar.riveland@gamil.com>
2024-01-26 10:42:15 +0100

    test combo perf

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ from instructRNN.tasks.tasks import SWAPS_DICT
 from instructRNN.analysis.model_analysis import get_holdout_CCGP, get_multitask_CCGP,  get_model_clusters
 from instructRNN.analysis.model_eval import get_val_perf, get_multi_all_comp_perf, get_holdout_all_comp_perf
 from instructRNN.analysis.decoder_analysis import decoder_pipeline
+from ruleLearning.combo_eval import get_holdout_combo_perfs
 
 
 if __name__ == "__main__":
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('--overwrite', default=False, action='store_true', help='whether or not to overwrite existing files')
     parser.add_argument('--decode_embeddings', default=False, action='store_true', help='use a decoder that directly maps embeddings to instructions')
     parser.add_argument('--job_index', type=int, help='for use with slurm sbatch script, indexes the combination of seed and holdout tasks along with the model')
+
     args = parser.parse_args()
 
     MODEL_FOLDER = args.folder
@@ -44,39 +46,49 @@ if __name__ == "__main__":
         
             print('processing sm holdouts and decoder holdouts')
             decoder_pipeline(MODEL_FOLDER+'/swap_holdouts', args.models[0], sm_holdout=True, decoder_holdout=True)
-
-    jobs = make_analysis_jobs(args.models, args.seeds, args.layers, args.job_index)
-    for job in jobs: 
-        _seed, model, layer = job
-        if (model in full_models.big_models or 'sbertNetL' in model) and isinstance(layer, int):
-            layer += 12
+    
+    elif args.mode == 'combo_eval': 
+        n_components=list(range(10, 50, 5))
+        jobs = make_analysis_jobs(args.models, args.seeds, n_components, args.job_index)
+        for job in jobs: 
+            _seed, model, n_components = job
+            get_holdout_combo_perfs(model, _seed, n_components=n_components)
         
-        print(EXP_FOLDER)
-        print(model)
-        print(_seed)
-
-        if args.mode == 'holdout_ccgp':
-            if not os.path.exists(EXP_FOLDER+'/CCGP_scores/'+model+'/'+'layer'+str(layer)+'_task_holdout_seed'+str(_seed)+'.npy') or args.overwrite:
-                print('analyzing ccgp for layer '+str(layer), flush=True)
-                get_holdout_CCGP(EXP_FOLDER, model, _seed, layer= layer, save=True)
-            else:
-                print('Already trained: '+EXP_FOLDER+'/'+'layer'+str(layer)+'_task_holdout_seed'+str(_seed), flush=True)
-                continue
-
-        if args.mode == 'swap_ccgp':
-            get_holdout_CCGP(EXP_FOLDER, model, _seed, layer= 'task', instruct_mode='swap_combined', save=True)
-
-        elif args.mode == 'multi_ccgp': 
-            get_multitask_CCGP(EXP_FOLDER, model, _seed, layer= 'task', save=True)
-
-        elif args.mode == 'val':
-            get_val_perf(EXP_FOLDER, model, _seed, save=True)
-
-        elif args.mode == 'clusters':
-            get_model_clusters(EXP_FOLDER, model, _seed, save=True)
 
 
+    else: 
+        jobs = make_analysis_jobs(args.models, args.seeds, args.layers, args.job_index)
+        for job in jobs: 
+            _seed, model, layer = job
+            if (model in full_models.big_models or 'sbertNetL' in model) and isinstance(layer, int):
+                layer += 12
+            
+            print(EXP_FOLDER)
+            print(model)
+            print(_seed)
 
-                
+            if args.mode == 'holdout_ccgp':
+                if not os.path.exists(EXP_FOLDER+'/CCGP_scores/'+model+'/'+'layer'+str(layer)+'_task_holdout_seed'+str(_seed)+'.npy') or args.overwrite:
+                    print('analyzing ccgp for layer '+str(layer), flush=True)
+                    get_holdout_CCGP(EXP_FOLDER, model, _seed, layer= layer, save=True)
+                else:
+                    print('Already trained: '+EXP_FOLDER+'/'+'layer'+str(layer)+'_task_holdout_seed'+str(_seed), flush=True)
+                    continue
+
+            if args.mode == 'swap_ccgp':
+                get_holdout_CCGP(EXP_FOLDER, model, _seed, layer= 'task', instruct_mode='swap_combined', save=True)
+
+            elif args.mode == 'multi_ccgp': 
+                get_multitask_CCGP(EXP_FOLDER, model, _seed, layer= 'task', save=True)
+
+            elif args.mode == 'val':
+                get_val_perf(EXP_FOLDER, model, _seed, save=True)
+
+            elif args.mode == 'clusters':
+                get_model_clusters(EXP_FOLDER, model, _seed, save=True)
+
+
+
+                    
 
 
---
+++ ruleLearning/combo_eval.py
@@ -0,0 +1,100 @@
+from instructRNN.analysis.model_analysis import *
+from instructRNN.tasks.tasks import TASK_LIST
+from instructRNN.tasks.task_criteria import isCorrect
+from instructRNN.models.full_models import make_default_model
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans 
+
+import torch
+import torch.nn as nn
+
+##############RULE CLUSTER####################
+
+def get_directions(rule_reps):
+    directions_list = []
+    directions_labels = []
+
+    for key, values in DICH_DICT.items():
+        directions_list.extend([rule_reps[TASK_LIST.index(dich_pair[1])]-rule_reps[TASK_LIST.index(dich_pair[0])] for dich_pair in values])
+        directions_labels.extend(values)
+
+    directions_array = np.array(directions_list)
+    return directions_labels, directions_array
+
+def get_held_in_indices(swap_label): 
+    int_list = list(range(50))
+    [int_list.remove(x) for x in [TASK_LIST.index(task) for task in SWAPS_DICT[swap_label]]]
+    return int_list
+    
+def get_holdout_rule_reps(model_name, swap_label, seed): 
+    model = make_default_model(model_name)
+    model.load_model(f'NN_simData/swap_holdouts/{swap_label}/{model_name}', suffix=f'_seed{seed}')
+    held_in_indices = get_held_in_indices(swap_label)    
+
+    if hasattr(model, 'langModel'): 
+        rule_reps = get_instruct_reps(model.langModel)
+    elif hasattr(model, 'rule_encoder'): 
+        rule_reps = get_rule_reps(model, use_rule_encoder=True)
+    else: 
+        rule_reps = get_rule_reps(model)
+        
+    return rule_reps[held_in_indices, ...]
+
+def gen_task_transitions(rule_reps, num_transitions=500):
+    transitions = []
+    for _ in range(num_transitions): 
+        task_draw = np.random.randint(rule_reps.shape[0], size=2)
+        instruct_draw = np.random.randint(rule_reps.shape[1], size=2)
+        transitions.append(rule_reps[task_draw[0], instruct_draw[0], : ]-rule_reps[task_draw[1], instruct_draw[1], :])
+    return np.array(transitions)
+
+def get_combo_mat(rule_reps, clusters):
+    comb_mat = np.empty((len(rule_reps), len(clusters), 64))
+    for i, rep in enumerate(rule_reps.mean(1)): 
+        for j, direc in enumerate(clusters): 
+            comb_mat[i, j, :] = rep+direc
+    
+    return comb_mat
+    
+def eval_task_combos(model, combos, num_repeats = 25, tasks=TASK_LIST): 
+    all_correct_array=np.empty((len(tasks), num_repeats, combos.shape[0], combos.shape[1]))
+    with torch.no_grad(): 
+        for i, task in enumerate(tasks): 
+            print(f'processing task {task}')
+            for j in range(num_repeats): 
+                print(j)
+                rules = torch.tensor(combos.reshape(-1, 64)).to(model.__device__)
+                ins, targets, _, target_dirs, _ = construct_trials(task, rules.shape[0])
+                out, _ = model(torch.Tensor(ins).to(model.__device__), info_embedded=rules)
+                corrects = isCorrect(out, torch.Tensor(targets), target_dirs)
+                reshaped_correct = corrects.reshape(combos.shape[0], combos.shape[1])
+                all_correct_array[i, j , :, :] = reshaped_correct
+
+    return all_correct_array
+
+
+def get_holdout_combo_perfs(model_name, seed, n_components=25, num_repeats=25, save=True):
+    all_correct_array=np.zeros((5, len(TASK_LIST), num_repeats, 45, n_components))
+
+    model = make_default_model(model_name)
+    print(f'\n PROCESSING SEED {seed} \n')
+    for swap_label, holdouts in SWAPS_DICT.items(): 
+        rule_reps = get_holdout_rule_reps(model_name, swap_label, seed)
+        task_transition_data = gen_task_transitions(rule_reps)
+
+        k_means = KMeans(n_clusters=n_components)
+        k_means.fit(task_transition_data)
+        clusters = k_means.cluster_centers_
+
+        combo_mat = get_combo_mat(rule_reps, clusters)
+        evals = eval_task_combos(model, combo_mat, num_repeats = num_repeats, tasks=holdouts)
+        all_correct_array[seed, [TASK_LIST.index(task) for task in holdouts], ...] = evals
+
+    if save:
+        np.save(f'rule_combo_perf/{model_name}_n{n_components}_seed{seed}', all_correct_array)
+    return all_correct_array
